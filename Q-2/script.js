7b1bfb7af5f6cc982d64c2839691725b6393ccdb
samayaraj2026 <samayaraj.m@mitrahsoft.in>
2026-03-16 17:07:02 +0530

    Second Task

--- Q-2/script.js
+++ Q-2/script.js
@@ -11,26 +11,27 @@ const inputBox = document.getElementById('input-box');
 const error = document.getElementById('error')
 const result = document.getElementById('result')
 
-function Check(){
+function Check() {
     const inputBoxVal = inputBox.value;
 
-    if(!inputBoxVal){
+
+    error.innerText = " ";
+    error.innerText = " ";
+
+    if (!inputBoxVal) {
         error.innerText = "Please Enter Text"
         return
     }
 
-    error.innerText = " ";
-
-    if(inputBoxVal.length<=1){
-        if(inputBoxVal === 'a' || 'e' || 'i' || 'o' || 'u' || 'A' || 'E' || 'I' || 'O' || 'U'){
-            result.innerHTML  = `${inputBoxVal} is a Vowel`;
+    if (inputBoxVal.length === 1) {
+        // Correct way to check vowels
+        if ('aeiouAEIOU'.includes(inputBoxVal)) {
+            result.innerText = `${inputBoxVal} is a Vowel`;
+        } else {
+            result.innerText = `${inputBoxVal} is a Not Vowel`;
         }
-        else{
-            result.innerHTML  = `${inputBoxVal} is a Not Vowel`;
-        }
+    } else if (inputBoxVal.length > 1) {
+        // Proper length check
+        result.innerText = 'Enter single character';
     }
-    else{
-        
-    }
-
 }
